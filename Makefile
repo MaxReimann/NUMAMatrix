9422717626391f1802afe7b603bff791993b6af3
MaxReimann <max.reimann@student.hpi.uni-potsdam.de>
2014-12-17 22:11:19 +0100

    add testfunction

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
-TARGET = matrixmult
+TARGET = bin/matrixmult
 LIBS = -lm
 CC = gcc
 CFLAGS = -g -Wall
 
 .PHONY: default all clean
 
-default: $(TARGET)
+
+default: clean $(TARGET) run
 all: default
+
+run: $(TARGET)
+	./matrixmult
+
 
 OBJECTS = $(patsubst %.c, %.o, $(wildcard *.c))
 HEADERS = $(wildcard *.h)
--- naiveMult.c
+++ naiveMult.c
@@ -2,15 +2,17 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define ndim  256
-#define mdim  256
+#define ndim  512
+#define mdim  512
 
 #define IDX(Y, X) (ndim * Y + X) //rows first
+
+typedef enum { false, true } bool;
 
 void multiplyPart(int sI, double first[], double second[], double output[])
 {
   int x,y;
-    for (x = 0; x < ndim; x++ )
+    for (x = 0; x < ndim; x++)
     {
       for (y = 0; y < mdim; y++)
         output[IDX(x,y)] += first[IDX(x,sI)] * second[IDX(sI,y)];
@@ -18,6 +20,35 @@ void multiplyPart(int sI, double first[], double second[], double output[])
 }
 
 
+bool isValid(double first[], double second[], double multiplied[])
+{
+    
+    int c, d, k;
+    double sum = 0;
+    bool valid = true;
+    //standard matrix multiplication
+    for (c = 0; c < ndim; c++)
+    {
+      for (d = 0; d < mdim; d++)
+      {
+        for (k = 0; k < ndim; k++)
+        {
+          sum = sum + first[IDX(c,k)]*second[IDX(k,d)];
+        }
+ 
+        if (multiplied[IDX(c,d)] != sum)
+        {
+          valid = false;
+          printf("result matrix not valid in row %d, col %d \n", c, d);
+          break;
+        }
+
+        sum = 0;
+      }
+    }
+
+    return valid;
+}
 
 
 int main()
@@ -27,7 +58,6 @@ int main()
 
 
   int c, d, k, sI;
-  long sum = 0;
   double *first=malloc(ndim*mdim*sizeof(double));
   double *second=malloc(ndim*mdim*sizeof(double));
   double *multiply=malloc(ndim*mdim*sizeof(double));
@@ -49,25 +79,16 @@ int main()
     multiplyPart(sI,first,second,multiply);
   }
 
-/*    for ( c = 0 ; c < ndim ; c++ )
-    {
-      for ( d = 0 ; d < mdim ; d++ )
-      {
-        for ( k = 0 ; k < ndim ; k++ )
-        {
-          sum = sum + first[c][k]*second[k][d];
-        }
- 
-        multiply[c][d] = sum;
-        sum = 0;
-      }
-    }*/
-
   clock_t end = clock();
   float seconds = (float)(end - start) / CLOCKS_PER_SEC;
 
-  printf("matrix multiplication took: %f", seconds);
-  printf("\n finished \n");
+  printf("matrix multiplication took: %f\n", seconds);
+
+  if (isValid(first, second, multiply))
+    printf("valid matrix multiplication\n");
+
+
+  printf("finished \n");
 
   return 0;
 }
