9ea882c9c3a8c9779d44b8fc7b99bbbefb5fa8df
unknown <max.reimann@student.hpi.uni-potsdam.de>
2014-12-17 15:53:43 +0100

    heap array

--- naiveMult.c
+++ naiveMult.c
@@ -2,18 +2,21 @@
 #include <time.h>
 #include <stdlib.h>
 
+const int ndim = 1000;
+const int mdim = 1000;
 
 int main()
 {
 
   srand(time(NULL));
-  const int ndim = 10;
-  const int mdim = 10;
 
 
   int c, d, k;
   long sum = 0;
-  int first[ndim][mdim], second[ndim][mdim], multiply[ndim][mdim];
+  int (*first)[ndim]=malloc(ndim*mdim*sizeof(int));
+  int (*second)[ndim]=malloc(ndim*mdim*sizeof(int));
+  int (*multiply)[ndim]=malloc(ndim*mdim*sizeof(int));
+  //  int **first, second[ndim][mdim], multiply[ndim][mdim];
 
 
   for (c = 0 ; c < ndim; c++)
@@ -26,6 +29,7 @@ int main()
   
   }
 
+  clock_t start = clock();
 
     for ( c = 0 ; c < ndim ; c++ )
     {
@@ -41,17 +45,21 @@ int main()
       }
     }
 
+  clock_t end = clock();
+  float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+
+  printf("matrix multiplication took: %f", seconds);
  
-    for ( c = 0 ; c < ndim ; c++ )
-    {
-      for ( d = 0 ; d < mdim ; d++ )
-      {
-        printf("%d\t", multiply[c][d]);
-      }
+    // for ( c = 0 ; c < ndim ; c++ )
+    // {
+    //   for ( d = 0 ; d < mdim ; d++ )
+    //   {
+    //     printf("%d\t", multiply[c][d]);
+    //   }
  
-      printf("\n");
-    }
-    printf("finished \n");
+    //   printf("\n");
+    // }
+    printf("\n finished \n");
 
   return 0;
 }
